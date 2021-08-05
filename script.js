e722cdbbfc78ca8e92c7db79a0a0ce01cdbd5a50
whosudh <88132972+whosudh@users.noreply.github.com>
2021-08-05 16:47:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let a=20;
+let b=10;
+// ADDITION
+console.log(`The addition of a and b is ${a+b}`);
+// SUBTRACTION
+console.log(`The subtraction of a and b is ${a-b}`);
+// MULTIPLICATION
+console.log(`The multiplication of a and b is ${a*b}`);
+// DIVISION
+console.log(`The division of a and b is ${a/b}`);
